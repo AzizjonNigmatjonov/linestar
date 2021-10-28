8036a30cbb9a65482f3682895c598b9d23b69950
Azizjon Nigmatjonov <74749706+AzizjonNigmatjonov@users.noreply.github.com>
2021-10-28 09:04:51 +0500

    added

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,23 @@ window.onload = function() {
     const el = document.getElementById('overlay');
     el.style.display = 'none';
 };
+
+const servicesItems = document.querySelectorAll('.services-item');
+const urlItem = window.location.hash.substr(1);
+servicesItems.forEach(function(item) {
+    if (item.id == urlItem) {
+        item.classList.add('active');
+    } else {
+        item.classList.remove('active');
+    }
+    const serviceItemLinks = document.querySelectorAll('.services-item .service-link p');
+    serviceItemLinks.forEach(function(link) {
+        link.addEventListener('click', event => {
+            if (event.target.dataset.target == item.id) {
+                item.classList.add('active');
+            } else {
+                item.classList.remove('active');
+            }
+        })
+    });
+})
